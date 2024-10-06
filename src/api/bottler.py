9eb2fef783a0eec4b9a6c2162c6237056bd8b76c
Samiksha <samiksha.gk29@gmail.com>
2024-10-06 14:54:44 -0700

    changed bottle logic

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -18,21 +18,33 @@ class PotionInventory(BaseModel):
 @router.post("/deliver/{order_id}")
 def post_deliver_bottles(potions_delivered: list[PotionInventory], order_id: int):
     """ """
-    print(f"potions delievered: {potions_delivered} order_id: {order_id}")
 
-    total_potions = 0
-    total_ml = 0
+    green_potions, red_potions, blue_potions = 0, 0, 0
+    green_ml, red_ml, blue_ml = 0, 0, 0
     for potion in potions_delivered:
+        if potion.potion_type == [100, 0, 0, 0]:
+            red_potions += potion.quantity
+            red_ml += red_potions * 100
         if potion.potion_type == [0, 100, 0, 0]:
-            total_potions += potion.quantity
-            # total_ml += potion.potion_type[1] * potion.quantity
+            green_potions += potion.quantity
+            green_ml += green_potions * 100
+        if potion.potion_type == [0, 0, 100, 0]:
+            blue_potions += potion.quantity
+            blue_ml += blue_potions * 100
 
     with db.engine.begin() as connection:
         connection.execute(sqlalchemy.text(
             "UPDATE global_inventory \
-                SET num_green_potions = num_green_potions + :total_potions, \
-                    num_green_ml = num_green_ml - :total_potions * 100"),
-                                           {"total_potions": total_potions})
+            SET num_green_potions = num_green_potions + :gp, \
+                num_red_potions = num_red_potions + :rp, \
+                num_blue_potions = num_blue_potions + :bp, \
+                num_green_ml = num_green_ml - :greenml, \
+                num_red_ml = num_red_ml - :redml, \
+                num_blue_ml = num_blue_ml - :blueml"),
+                {"gp": green_potions, "rp": red_potions, "bp": blue_potions, \
+                 "greenml": green_ml, "redml": red_ml, "blueml": blue_ml})
+        
+        print(f"potions delievered: {potions_delivered} order_id: {order_id}")
     
     return "OK"
 
@@ -46,21 +58,41 @@ def get_bottle_plan():
     # green potion to add.
     # Expressed in integers from 1 to 100 that must sum up to 100.
 
-    # Initial logic: bottle all barrels into red potions.
-
     with db.engine.begin() as connection:
-        result = connection.execute(sqlalchemy.text("SELECT num_green_ml FROM global_inventory"))
+        result = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory")).mappings()
+        result = result.fetchone()
+
+    plan = []
+    greenml = result["num_green_ml"]
+    redml = result["num_red_ml"]
+    blueml = result["num_blue_ml"]
+    print("green: ", greenml)
+    print("red: ", redml)
+    print("blue: ", blueml)
+
+    if greenml:
+        num_green = int(greenml/100)
+        plan.append({
+            "potion_type": [0, 100, 0, 0],
+            "quantity": num_green
+        })
     
-    green = result.first().num_green_ml
-    num_green = int(green/100)
-    if num_green:
-        return [
-                {
-                    "potion_type": [0, 100, 0, 0],
-                    "quantity": num_green,
-                }
-            ]
-    return[]
+    if redml:
+        num_red = int(redml/100)
+        plan.append({
+            "potion_type": [100, 0, 0, 0],
+            "quantity": num_red
+        })
+
+    if blueml:
+        num_blue = int(blueml/100)
+        plan.append({
+            "potion_type": [0, 0, 100, 0],
+            "quantity": num_blue
+        })
+
+    print("plan: ", plan)
+    return plan
 
 if __name__ == "__main__":
     print(get_bottle_plan())
