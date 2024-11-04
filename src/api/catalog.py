0ba7c221a1dc3654846497529c439ff58c34a04b
Samiksha <samiksha.gk29@gmail.com>
2024-11-03 19:05:14 -0800

    fix catalog to work with ledgerized potion table

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -13,23 +13,37 @@ def get_catalog():
     """
 
     with db.engine.begin() as connection:
-        result = connection.execute(sqlalchemy.text("SELECT * FROM potions WHERE inventory > 0 LIMIT 6")).mappings()
-        result = result.fetchall()
+        potions = connection.execute(sqlalchemy.text("""SELECT * FROM potions""")).mappings().fetchall()
         
     catalog = []
+    potion_dict = {}
 
-    for potion in result:
+    for potion in potions:
         type = [potion["red_amt"], potion["green_amt"], potion["blue_amt"], potion["dark_amt"]]
-        catalog.append(
-            {
-                "sku": potion["sku"],
-                "name": potion["sku"],
-                "quantity": potion["inventory"],
-                "price": potion["price"],
-                "potion_type": type
-            }
-        )
+        sku =  potion["sku"]
+        quantity = potion["inventory"]
+        if sku in potion_dict:
+            qty = int(potion_dict[sku]["quantity"])
+            qty += quantity
+            potion_dict[sku]["quantity"] = qty
+        else:
+            potion_dict[sku] = {
+            "sku": potion["sku"],
+            "name": potion["sku"],
+            "quantity": potion["inventory"],
+            "price": potion["price"],
+            "potion_type": type
+        }
+            
+    print(f"Potion dict: {potion}")
+
+    catalog = list(potion_dict.values())
+    for potion in catalog:
+        if potion["quantity"] == 0:
+            catalog.remove(potion)
+
+    if len(catalog) > 6:
+        catalog = catalog[:6]
 
     print ("Catalog: ", catalog)
-
     return catalog
